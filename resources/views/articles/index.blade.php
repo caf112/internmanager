a255db5b70fa0407cb9ae1e848620f0416de9f65
kazama0812 <kazamaaa0812@gmail.com>
2023-09-06 14:29:41 +0900

    modify:indexの見た目と、articleのクラス名修正

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -19,11 +19,11 @@
 <input type="button" onclick="location.href='/programs'" value="スケジュール">
 <p><a href="{{ route('articles.create') }}">記録を書く</a></p>
         @foreach ($articles as $article)
-        <article class="article-item">
-            <div class="article-title">
+        <article class="program-item">
+            <div class="program-title">
             <a href="{{ route('articles.show', $article) }}">{{ $article->title }}</a>   
             </div>
-            <div class="article-date">{{ $article->date }}</div>
+            <div class="program-date">{{ $article->date }}</div>
         </article>
         @endforeach
         @if (count($articles) === 0)
