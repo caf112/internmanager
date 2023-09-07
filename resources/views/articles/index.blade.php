da50e86b7ed8e58ea279415309a2b2646e890f6b
kazama0812 <kazamaaa0812@gmail.com>
2023-09-07 11:48:14 +0900

    modify:ボタンをかわいくして、サイドバー整えた

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -2,17 +2,34 @@
 @section('head')
 
 <h1 class='app-title'>Intern Manager</h1>
-<a href="/articles" class="site-title">企業一覧</a>
+
 @endsection()
 
-@section('aside')<!--サイドバー-->
+@section('aside')<!--サイドバーーーーーーーーーーーーーーーーーーーーーーーーーーーーー-->
 
-あいうえお
-@endsection
+<!--新規作成ボタン-->
 
-@section('content')
+<a href="{{ route('articles.create') }}">
+<button class="create-button" >
+  <span>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"></path><path fill="currentColor" d="M11 11V5h2v6h6v2h-6v6h-2v-6H5v-2z"></path></svg> New Data
+  </span>
+</button>
+</a>
+<br>
+<a href="/programs" class="intern-index">参加予定インターン</a>
+<br>
+<a href="/articles" class="intern-index">参加済みインターン</a>
+
+
+ <!--検索欄-->
 <form method="GET" action="{{ route('articles.search') }}">
-    <input type="search" placeholder="企業名を入力" name="search" value="@if (isset($search)) {{ $search }} @endif">
+   <!--<input type="search" placeholder="企業名を入力" name="search" value="@if (isset($search)) {{ $search }} @endif">-->
+    <div class="group">
+     <svg class="icon" aria-hidden="true" viewBox="0 0 24 24"><g><path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path></g></svg>
+     <input type="search" placeholder="企業名を入力" class="serch-input" name="search" value="@if (isset($search)) {{ $search }} @endif">
+    </div>
+
     <div>
         <button type="submit">検索</button>
         <button>
@@ -22,8 +39,12 @@
         </button>
     </div>
 </form>
+
+
 <form action="{{ route('industryFilter') }}" method="POST">
     @csrf
+
+    <p>業種別に検索</p>
     <select name="industry" id="industryFilter">
         <option value="all">全て</option>
         <option value="農林・水産">農林・水産</option>
@@ -40,10 +61,12 @@
                 <option value="サービス業">サービス業</option>
                 <option value="分類不能産業">分類不能産業</option>
     </select>
-    <button type="submit">絞り込み</button>
+    <button type="submit" class="filter-button">絞り込み</button>
 </form>
 <form action="{{ route('periodFilter') }}" method="POST">
     @csrf
+
+    <p>期間別に検索</p>
     <select name="period" id="periodFilter">
         <option value="all">全て</option>
         <option value="P1">1Day</option>
@@ -51,15 +74,23 @@
                 <option value="P3">短期</option>
                 <option value="P4">長期</option>
     </select>
-    <button type="submit">絞り込み</button>
+    <button type="submit" class="filter-button">絞り込み</button>
 </form>
 <a href="{{ route('selectionFilter', ['selection' => 'all']) }}">全て</a>
 <a href="{{ route('selectionFilter', ['selection' => 'S1']) }}">選考なし</a>
 <a href="{{ route('selectionFilter', ['selection' => 'S2']) }}">選考あり</a>
 <a href="{{ route('selectionFilter', ['selection' => 'S3']) }}">選考落ち</a>
 
-<input type="button" onclick="location.href='/programs'" value="スケジュール">
-<p><a href="{{ route('articles.create') }}">記録を書く</a></p>
+
+
+
+@endsection<!--サイドバーここまでーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー-->
+
+
+
+
+
+@section('content')<!--一覧ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー-->
         @foreach ($articles as $article)
         <article class="program-item">
             <div class="program-title">
@@ -71,4 +102,4 @@
         @if (count($articles) === 0)
             <p>No results found for "{{ $search }}"</p>
         @endif
-@endsection()
+@endsection()<!--一覧ここまでーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー-->
